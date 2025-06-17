11ac50c407d7db75d33d9208aaaf6cd0ff3edd04
Ashoka R <ashoka6031@gmail.com>
2025-06-17 10:43:05 +0530

    Add files via upload

---
+++ dist/main.js
@@ -0,0 +1,4 @@
+
+document.getElementById("book").addEventListener("click", () => {
+  window.open("https://wa.me/919663427720?text=Hi%20Trim%20Buddy%2C%20I%20would%20like%20to%20book%20a%20haircut%20at%20home.", "_blank");
+});
